7cf98213937c08b347fc606fda4e6c5fc1f521c7
Fuky9 <lukasfukan6@gmail.com>
2024-04-05 22:36:40 +0200

    Fixed the database name to the right database

--- django_project/settings.py
+++ django_project/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'blog_db',
+        'NAME': 'eventgatordb',
         'HOST' : '127.0.0.1',
         'PORT' : '3306',
         'USER' : 'root',
